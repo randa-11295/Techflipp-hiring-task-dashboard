7fe47db1f4525ca9147e039781748107ad582f95
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-06-12 16:24:18 +0300

    add mui and theme

---
+++ src/app/utilities/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#9A69DB', 
+    },
+
+  },
+
+});
+
+export default theme;
